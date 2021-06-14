fbe7734b23c0beb09a6d2f4a2d460e262321c522
Mason Ginter <mason@dagint.com>
2021-06-14 17:16:03 -0400

    Organization - Use my custom gba-irq crate - Change opt-level to 1 - Organize init registers into 1 fn - Add more comments

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #![no_std]
 #![no_main]
 
-use gba::{debug, fatal, prelude::*, warning};
+use gba::{fatal, prelude::*};
 use gba_dvd::prelude::*;
+use gba_irq::prelude::IRQHandler;
 
 #[panic_handler]
 #[allow(unused)]
@@ -10,19 +11,41 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
   fatal!("{}", info);
 }
 
+fn init_reg_values() {
+  // Init DisplayControl
+  const SETTING: DisplayControl = DisplayControl::new()
+    .with_display_mode(0)
+    .with_display_bg0(true)
+    .with_display_obj(true)
+    .with_obj_vram_1d(true);
+  DISPCNT.write(SETTING);
+  // Set the IRQ handler to use.
+  unsafe { USER_IRQ_HANDLER.write(Some(Handler::irq_handler_a32)) };
+  const VBLANK_FLAG: InterruptFlags = InterruptFlags::new()
+    .with_vblank(true);
+  // Enable all interrupts that are set in the IE register.
+  unsafe { IME.write(true) };
+  unsafe { IE.write(VBLANK_FLAG) };
+
+  // Enable vblank irq
+  const DISPLAY_SETTINGS: DisplayStatus = DisplayStatus::new()
+    .with_vblank_irq_enabled(true);
+  DISPSTAT.write(DISPLAY_SETTINGS);
+}
+/// Sprites are not hidden by default, so hide them
 fn hide_all_objs() {
   for i in 0..128 {
     OAM_ATTR0.index(i).write(ObjAttr0::new().with_double_disabled(true));
   }
 }
-
+/// Copy palette for DVD to VRAM
 fn copy_pal() {
   // Load palette into vram
   for (i, color) in DVD_PAL.iter().enumerate() {
     OBJ_PALETTE.index(i).write(Color(*color));  
   }
 }
-
+/// Copy DVD tiles to VRAM
 fn copy_tiles() {
   let dvd = DVD_IMG;
   // Load tiles into vram
@@ -38,33 +61,19 @@ fn copy_tiles() {
 }
 #[no_mangle]
 pub fn main() -> ! {
-  const SETTING: DisplayControl = DisplayControl::new()
-    .with_display_mode(0)
-    .with_display_bg0(true)
-    .with_display_obj(true)
-    .with_obj_vram_1d(true);
-  DISPCNT.write(SETTING);
-  const VBLANK_FLAG: InterruptFlags = InterruptFlags::new().with_vblank(true);
-
-  // Set the IRQ handler to use.
-  unsafe { USER_IRQ_HANDLER.write(Some(irq_handler_a32)) };
-
-  // Enable all interrupts that are set in the IE register.
-  unsafe { IME.write(true) };
-
-  // Enable vblank irq
-  const DISPLAY_SETTINGS: DisplayStatus = DisplayStatus::new()
-    .with_vblank_irq_enabled(true);
-  DISPSTAT.write(DISPLAY_SETTINGS);
-
+  init_reg_values();
+  // Set background color
   BG_PALETTE.index(0).write(Color::from_rgb(3, 3, 3));
+  // OBJs are not hidden by default
   hide_all_objs();
+  // Copy dvd data to VRAM
   copy_pal();
   copy_tiles();
-  // Size should be 64x48 (from top left), but takes up a full 64x64 sprite
+  
   let mut x_dir = Direction::RIGHT;
   let mut y_dir = Direction::UP;
   let window = BoundingBox::new().with_size(240, 160);
+  // Size should be 64x48 (from top left), but takes up a full 64x64 sprite
   let mut dvd_obj = Obj::new(40, 30, 64, 48);
   dvd_obj.set_size(&SpriteDimensions::Size64x64);
   dvd_obj.set_tile_ind(0);
@@ -72,18 +81,17 @@ pub fn main() -> ! {
 
   dvd_obj.copy_to_block(0);
   
-  // corner counter will count down to zero each frame 
-  //    and will be reset if the dvd hits a corner
+  // corner counter will be reset if the dvd hits a corner
+  //    and will count down to zero each frame 
   let mut corner_counter = 0;
+  // Init rng
   let mut rng = gba::random::RNG::default();
   loop {
     // Do logic
-    // dvd_obj.move_pos(x_dir.value() as i16, y_dir.value() as i16);
     dvd_obj.move_pos(x_dir.value(), y_dir.value());
 
     let collisions = dvd_obj.bounding_box.is_outside(window);
     if collisions.num_collisions() >= 1 {
-      debug!("Position: {} {}", dvd_obj.x(), dvd_obj.y());
       if collisions.top() || collisions.bottom() {
         y_dir = y_dir.flip(); 
       }
@@ -94,7 +102,6 @@ pub fn main() -> ! {
       OBJ_PALETTE.index(1).write(rng.next_color());
       // Corner hit
       if collisions.num_collisions() == 2 {
-        warning!("Corner Hit!");
         corner_counter = 80;
       }
     }
@@ -107,15 +114,9 @@ pub fn main() -> ! {
       // Decrease counter
       corner_counter -= 1;
     }
-    // now we wait
-    // spin_until_vblank();
-    
-    // Draw things
-    unsafe { IE.write(VBLANK_FLAG) };
-
-    // now we wait again
-    // spin_until_vdraw();
+    // Wait for VBlank
     unsafe { VBlankIntrWait(); }
+    // Copy dvd OBJ data during Vblank
     dvd_obj.copy_to_block(0);
   }
 }
--- src/misc.rs
+++ src/misc.rs
@@ -1,5 +1,2 @@
 mod direction;
 pub use direction::*;
-
-mod vblank;
-pub use vblank::*;
