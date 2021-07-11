b00d15e11155a1d89f46fa93060204e3950d59d1
Mason Ginter <mason@dagint.com>
2021-07-11 04:08:43 -0400

    Add some info about build.rs

--- build.rs
+++ build.rs
@@ -3,10 +3,11 @@ use gba_image_macros::prelude::*;
 
 fn main() {
     // Create a ConstWriter for the macros to use
-    let consts = ConstWriter::from_path(&Path::new("src/assets.rs")).unwrap();
-    let mut consts = consts.finish_dependencies();
+    let writer = ConstWriter::from_path(&Path::new("src/assets.rs")).unwrap();
+    let mut consts = writer.finish_dependencies();
     // add a load_sprite macro for each sprite
     load_sprite!(consts, "dvd", "DVD");
+    // Only update if this file or files in the asset folder update
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=assets");
 }
